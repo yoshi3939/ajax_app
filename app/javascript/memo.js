5dce4549d944936d77ca3be52b42fdba957992ca
yoshi3939 <irieyoshimi43@gmail.com>
2021-07-02 18:21:13 +0900

    コントローラーにリクエストを送信する

---
+++ app/javascript/memo.js
@@ -0,0 +1,14 @@
+function post (){
+  const submit = document.getElementById("submit");
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    const form = document.getElementById("form");
+    const formDate = new FormDate(form);
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+  });
+};
+
+window.addEventListener('load', post);
